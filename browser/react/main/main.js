0af13e770a0a4bd64cfe2b66d27d6245ed1cccee
Joe Bosman <joseph.a.bosman@gmail.com>
2017-06-09 16:49:06 -0400

    Added text to orbs.

--- browser/react/main/main.js
+++ browser/react/main/main.js
@@ -4,28 +4,37 @@ require('./main.scss');
 const blueOrb = require('./images/blueOrb_200x200.png');
 
 function orbs(){
-	let siteLocations = ['background', 'skills', 'projects', 'contact'];
+	let siteLocations = ['skills', 'projects', 'contact', 'background'];
 
 	return siteLocations.map( (location, i) => {
-		return <img 
-					key={`${i}`} 
-					onClick={() => { animate(i)}} 
-					className={`box rotate blueOrb${i}` }
+		return(
+			<div 
+				key={`${i}`} 
+				className={ `orb-container orb-${i}` }
+				onClick={() => { animate(i)} } 
+			>
+				<img 
 					src={blueOrb} 
 					alt='Blue Orb' 
 				/>
+				<h3 className='orb-text'>{location[0].toUpperCase() + location.slice(1)}</h3>
+			</div>
+		)
 	});
 }
 
 function animate(orbNumber){
-	let orbs = Array.prototype.slice.call(document.querySelectorAll('.box'));
+	let orbs = Array.prototype.slice.call(document.querySelectorAll('.orb-container'));
+	orbs[0].classList.toggle('rotateTop');
+	orbs[1].classList.toggle('rotateRight');
+	orbs[2].classList.toggle('rotateBottom');
 	orbs[3].classList.toggle('rotateLeft');
 }
 
 export default function(){
 	return (
 		<div id='main'>
-			<div id='orb-container'>
+			<div id='orb-group-container'>
 				{ orbs() }
 			</div>
 		</div>
