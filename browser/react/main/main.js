eb64c7c8cd1c06ec102d3c7293dfd9299045a715
Joe Bosman <joseph.a.bosman@gmail.com>
2017-06-09 21:41:43 -0400

    Updated with linear motion back to center after rotations.

--- browser/react/main/main.js
+++ browser/react/main/main.js
@@ -1,45 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 require('./main.scss');
 
 const blueOrb = require('./images/blueOrb_200x200.png');
+const rotateClasses = [ 'rotateTop', 'rotateRight', 'rotateBottom', 'rotateLeft'];
+const linearClasses = [ 'topToCenter', 'rightToCenter', 'bottomToCenter', 'leftToCenter'];
 
-function orbs(){
-	let siteLocations = ['skills', 'projects', 'contact', 'background'];
+export default class Main extends Component {
 
-	return siteLocations.map( (location, i) => {
-		return(
-			<div 
-				key={`${i}`} 
-				className={ `orb-container orb-${i}` }
-				onClick={() => { animate(i)} } 
-			>
-				<img 
-					src={blueOrb} 
-					alt='Blue Orb' 
-				/>
-				<h3 className='orb-text'>{location[0].toUpperCase() + location.slice(1)}</h3>
-			</div>
-		)
-	});
-}
+	componentDidMount(){
 
-function animate(orbNumber){
-	let orbs = Array.prototype.slice.call(document.querySelectorAll('.orb-container'));
-	orbs[0].classList.toggle('rotateTop');
-	orbs[1].classList.toggle('rotateRight');
-	orbs[2].classList.toggle('rotateBottom');
-	orbs[3].classList.toggle('rotateLeft');
-}
+		let orbs = Array.prototype.slice.call(document.querySelectorAll('.orb-container'));
 
-export default function(){
-	return (
+		orbs.forEach( (orb, i) => {
+			orb.addEventListener( 'animationend', function(){
+				this.classList.remove(rotateClasses[i]);	
+				
+				if( !this.classList.contains(linearClasses[i]) )
+					this.classList.add(linearClasses[i])
+			})
+		});
+	}
+
+	render(){
+		return (
 		<div id='main'>
 			<div id='orb-group-container'>
-				{ orbs() }
+				{ this.createOrbs() }
 			</div>
 		</div>
-	);
+		);
+	}
+
+	createOrbs(){
+		let siteLocations = ['skills', 'projects', 'contact', 'history'];
+
+		return siteLocations.map( (location, i) => {
+			return(
+				<div 
+					key={`${i}`} 
+					className={ `orb-container orb-${i}` }
+					onClick={() => { this.animate(i)} } 
+				>
+					<img 
+						src={blueOrb} 
+						alt='Blue Orb' 
+					/>
+					<h3 className='orb-text'>{location[0].toUpperCase() + location.slice(1)}</h3>
+				</div>
+			)
+		});
+	}
+
+	animate(orbNumber){
+		let orbs = Array.prototype.slice.call(document.querySelectorAll('.orb-container'));
+
+		orbs[orbNumber].setAttribute("style", "z-index: 1");
+
+		orbs.forEach( (orb, i) => { orb.classList.toggle(rotateClasses[i]); })
+	}
+
+
 }
+
 
 // <div className='box circle'></div>
 
