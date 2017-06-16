bea84f6c458e3841ca55ab6056bf67ab13d70704
Joe Bosman <joseph.a.bosman@gmail.com>
2017-06-16 11:17:16 -0400

    Added animation prefixes.

--- browser/react/main/sword-slashes/sword-slash-linear/sword-slash-linear.js
+++ browser/react/main/sword-slashes/sword-slash-linear/sword-slash-linear.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 require('./sword-slash-linear.scss');
 
 export default function SwordSlashLinear(props){
-	console.log(props)
 	return (
 		<div className={ `sword-slash-container ${props.className}` }>
 			<div className='sword-slash'></div>
